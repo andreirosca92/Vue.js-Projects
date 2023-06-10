705f717fba06a91cb72dd800fe6233bab1471e58
andrei <andreirosca92@gmail.com>
2023-06-10 15:39:20 +0200

    Counter-Vue.js 3

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
+  <Navbar />
   <h1>My first {{ title }}</h1>
+  <Counter />
+  <Footer />
 </template>
 
 <script>
+import Counter from "./components/Counter.vue";
+import Footer from "./components/Footer.vue";
+import Navbar from "./components/Navbar.vue";
+
 export default {
   name: "App",
   data() {
     return {
       title: "My first Vue App :)",
     };
+  },
+  components: {
+    Counter,
+    Footer,
+    Navbar,
   },
 };
 </script>
@@ -21,5 +33,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+    reset() {
+      this.count = 0;
+    },
+  },
+};
+</script>
+
+<template>
+  <p>You clicked me {{ count }} times.</p>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+  <button @click="reset">reset</button>
+</template>
